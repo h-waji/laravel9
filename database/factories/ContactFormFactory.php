4f470262b57482dd88ab4e44d77990be82eb26ba
wajima <goo.hiro.w@gmail.com>
2023-02-05 19:40:35 +0900

    Factory 追加

---
+++ database/factories/ContactFormFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFormFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(20),
+            'title' => $this->faker->realText(50),
+            'email' => $this->faker->email(),
+            'url' => $this->faker->url(),
+            'gender' => $this->faker->boolean(),
+            'age' => $this->faker->numberBetween(1, 6),
+            'contact' => $this->faker->realText(200),
+        ];
+    }
+}
